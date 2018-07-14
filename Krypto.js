9b5f8d65016a4aac434770f7caba612196ba7d3f
kribo852 <kribo852@student.liu.se>
2018-07-14 21:22:12 +0000

    stable on raspberry+midori

--- Krypto.js
+++ Krypto.js
@@ -1,5 +1,3 @@
-
-
 function makemap() {
 	var rtn = {};
 	for (var i = 255; i >= 0; i--) {
@@ -16,36 +14,23 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-/*function activeLetters(){
-	return ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä","Ö","!",".",""]; 
-}*/
-
-clearTextPhrase = () => 'KEEP IT LOUD ÄR AWESOME!! MATTECENTRUM GER ALLA SOM FRÅGAR EN TABLETTASK. ANALYS AV BOKSTÄVER, BOKSTAVSFÖLJDER OCH ORD ÄR ANVÄNDBARA FÖR ATT LÖSA ENKLA CHIFFER. JU LÄNGRE EN CHIFFER-TEXT ÄR DESTO MER ANVÄNDBAR INFORMATION INNEHÅLLER DEN';
+function clearTextPhrase() {
+	var sentence = Math.random();
+	if(sentence<0.33){
+		return 'KEEP IT LOUD ÄR AWESOME!! MATTECENTRUM GER ALLA SOM FRÅGAR EN TABLETTASK. ANALYS AV FREKVENSER FÖR BOKSTÄVER, BOKSTAVSFÖLJDER OCH ORD ÄR ANVÄNDBARA FÖR ATT LÖSA ENKLA CHIFFER. JU LÄNGRE EN CHIFFER-TEXT ÄR DESTO MER ANVÄNDBAR INFORMATION FÖR ATT LÖSA CHIFFRET INNEHÅLLER DEN.';
+	}
+	if(sentence<0.66){
+		return 'MATTECENTRUM ÄR EN RIKSTÄCKANDE DEMOKRATISK, POLITISKT OCH RELIGÖST OBUNDEN ORGANISATION SOM ANORDNAR INDIVIDUELL LÄXHJÄLP. BLI MEDLEM OCH FÅ MÖJLIGHET ATT PÅVERKA MATTECENTRUMS UTVECKLINGSARBETE. VARJE ÅR HAR MATTECENTRUM ÅRSMÖTE TILL VILKET ALLA MEDLEMMAR ÄR INBJUDNA. ALLA SOM VILL KAN BLI MEDLEMMAR, MER INFORMATION FINNS PÅ VÅR HEMSIDA';
+	}
+	return 'UTÖVER FREKVENSER FÖR BOKSTÄVER, BIGRAM OCH TRIGRAM, ALLTSÅ BOKSTAVSFÖLJDER MED 2 ELLER 3 BOKSTÄVER, KAN KONEXT ANVÄNDAS FÖR ATT LÖSA KRYPTON. TILL EXEMPEL ÄR ORDET MATTECENTRUM VANLIGT FÖREKOMMANDE I DE HÄR KRYPTONA. GENOM ATT GISSA ATT ETT ORD ÄR MATTECENTRUM FÅS EN STOR MÄNGD INFORMATION OM VAD DE ANDRA ORDEN KAN VARA'  
+}
 
 function isDecrypted(string1, string2){
 	return string1.toLowerCase()=== string2.toLowerCase();
 }
 
 function replace(lettermapping, correctletter, mappedletter){
-
 	var newlettermapping = Object.create(lettermapping);
 	newlettermapping[correctletter] = mappedletter;
 	return newlettermapping;
-
-}
-
-function mapFrequentLetters(lettermapping, cleartextphrase){
-	frequencies = ["E", "A", "N", "R", "T"];
-
-	letters = cleartextphrase.match(/.{1,1}/g);
-
-	
-
-	letters.forEach(letter => {
-		if(frequencies.indexOf(letter)!=-1) {
-
-		}
-	});
-
-
 }
